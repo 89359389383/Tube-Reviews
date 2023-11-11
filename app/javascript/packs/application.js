f385dd231957fbf6abbcebbc43ae30aa908acb85
NK <nba20093089@gmail.com>
2023-11-11 23:33:39 +0000

    動画詳細ページのUI改善,ソーシャルシェア機能の不具合残っている

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,9 @@ import './favorites';
 // Custom SCSSのインポート
 import '../stylesheets/custom.scss';
 
+// Social Share Buttonのインポート
+import "social-share-button";
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
