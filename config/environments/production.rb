810ea4eaaa7d4fbb6010b8ff119cd66316870b26
NK <nba20093089@gmail.com>
2024-06-08 23:40:50 +0000

    config/environments/production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,4 +113,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.hosts << "tube-reviews-1.onrender.com"
 end
