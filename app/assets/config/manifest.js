47d3f4eb331f713dccad489375d144f376095f45
NK <nba20093089@gmail.com>
2024-06-11 12:43:34 +0000

    manifest.jsにapplication.jsを追加

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,4 +2,6 @@
 //
 //= link_tree ../images
 //= link_directory ../stylesheets .scss
+//= link_directory ../javascripts .js
+//= link application.js
 //= link custom.css
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,36 @@
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
-//= require social-share-button
+// Rails UJSのインポートと起動
+import Rails from "@rails/ujs";
+Rails.start();
 
-document.addEventListener("turbolinks:load", function() {
-  try {
-    // シェアボタンの初期化や設定に関するコード
-    // 例: SocialShareButton.init();
-  } catch (e) {
-    console.error("シェアボタンの表示に失敗しました: ", e);
-  }
-});
+// jQueryをインポート
+import $ from 'jquery';
+
+// Bootstrapをインポート
+import "bootstrap"; 
+
+// favorites.jsをインポート
+import './favorites';
+
+// Custom SCSSのインポート
+import '../stylesheets/custom.scss';
+
+// Social Share Buttonのインポート
+import "social-share-button";
+
+import './folder_management';
+
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+// Uncomment to copy all static images under ./images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('./images', true)
+// const imagePath = (name) => images(name, true)
+
