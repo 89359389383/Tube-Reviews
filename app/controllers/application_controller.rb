f5342e2c9ab91ba2695a8e1ded0634d37ef09a98
NK <nba20093089@gmail.com>
2024-06-15 16:46:48 +0000

    本番環境におけるおすすめの動画が正しく表示されるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:new_guest]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # 例外処理の追加
